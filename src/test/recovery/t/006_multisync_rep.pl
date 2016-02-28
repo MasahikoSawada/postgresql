5abd801708421a59d3e2c9949f75cd0389bb46d5
Masahiko Sawada <sawada.mshk@gmail.com>
2016-02-27 21:45:58 -0400

    v14 : Fix character case.

--- src/test/recovery/t/006_multisync_rep.pl
+++ src/test/recovery/t/006_multisync_rep.pl
@@ -15,41 +15,41 @@ my $backup_name = 'my_backup';
 # Take backup
 $node_master->backup($backup_name);
 
-#Create standby1 linking to master
+# Create standby1 linking to master
 my $node_standby_1 = get_new_node('standby1');
 $node_standby_1->init_from_backup($node_master, $backup_name,
 								  has_streaming => 1);
 $node_standby_1->start;
 
 
-#Create standby2 linking to master
+# Create standby2 linking to master
 my $node_standby_2 = get_new_node('standby2');
 $node_standby_2->init_from_backup($node_master, $backup_name,
 								  has_streaming => 1);
 $node_standby_2->start;
 
-#Create standby3 linking to master
+# Create standby3 linking to master
 my $node_standby_3 = get_new_node('standby3');
 $node_standby_3->init_from_backup($node_master, $backup_name,
 								  has_streaming => 1);
 $node_standby_3->start;
 
-#Create standby4 linking to master
+# Create standby4 linking to master
 my $node_standby_4 = get_new_node('standby4');
 $node_standby_4->init_from_backup($node_master, $backup_name,
 								  has_streaming => 1);
 
-# check application sync_state on master initially
-my $result = $node_master->psql('postgres', "select application_name, sync_priority, sync_state from pg_stat_replication;");
+# Check application sync_state on master initially
+my $result = $node_master->psql('postgres', "SELECT application_name, sync_priority, sync_state FROM pg_stat_replication;");
 print "$result \n";
 is($result, "standby1|1|sync\nstandby2|2|potential\nstandby3|0|async", 'checked for standbys state for backward compatibility');
 
 
-#change the s_s_names = '*' and check sync state
-$node_master->psql('postgres', "alter system set synchronous_standby_names = '*';");
-$node_master->psql('postgres', "select pg_reload_conf();");
+# Change the s_s_names = '*' and check sync state
+$node_master->psql('postgres', "ALTER SYSTEM SET synchronous_standby_names = '*';");
+$node_master->psql('postgres', "SELECT pg_reload_conf();");
 
-$result = $node_master->psql('postgres', "select application_name, sync_priority, sync_state from pg_stat_replication;");
+$result = $node_master->psql('postgres', "SELECT application_name, sync_priority, sync_state FROM pg_stat_replication;");
 print "$result \n";
 is($result, "standby1|1|sync\nstandby2|1|potential\nstandby3|1|potential", 'checked for standbys state for backward compatibility with asterisk');
 
@@ -59,45 +59,43 @@ $node_standby_2->stop;
 $node_standby_3->stop;
 
 # Change the s_s_names = '2[standby1,standby2,standby3]' and check sync state
-$node_master->psql('postgres', "alter system set synchronous_standby_names = '2[standby1,standby2,standby3]';");
-$node_master->psql('postgres', "select pg_reload_conf();");
+$node_master->psql('postgres', "ALTER SYSTEM SET synchronous_standby_names = '2[standby1,standby2,standby3]';");
+$node_master->psql('postgres', "SELECT pg_reload_conf();");
 
-# standby2 and standby3 should be 'sync'
+# Standby2 and standby3 should be 'sync'
 $node_standby_2->start;
 $node_standby_3->start;
-$result = $node_master->psql('postgres', "select application_name, sync_priority, sync_state from pg_stat_replication;");
+$result = $node_master->psql('postgres', "SELECT application_name, sync_priority, sync_state FROM pg_stat_replication;");
 print "$result \n";
 is($result, "standby2|2|sync\nstandby3|3|sync", 'checked for sync standbys state transition 1');
 
 # Standby1 should be 'sync' instead of standby3, and standby3 should turn to 'potential'
 $node_standby_1->start;
 $node_standby_4->start;
-$result = $node_master->psql('postgres', "select application_name, sync_priority, sync_state from pg_stat_replication;");
+$result = $node_master->psql('postgres', "SELECT application_name, sync_priority, sync_state FROM pg_stat_replication;");
 print "$result \n";
 is($result, "standby2|2|sync\nstandby3|3|potential\nstandby1|1|sync\nstandby4|0|async", 'checked for sync standbys state transition 2');
 
-# change the s_s_names = '2[standby1,*,standby2]' and check sync state
-$node_master->psql('postgres', "alter system set synchronous_standby_names = '2[standby1,*,standby2]';");
-$node_master->psql('postgres', "select pg_reload_conf();");
+# Change the s_s_names = '2[standby1,*,standby2]' and check sync state
+$node_master->psql('postgres', "ALTER SYSTEM SET synchronous_standby_names = '2[standby1,*,standby2]';");
+$node_master->psql('postgres', "SELECT pg_reload_conf();");
 
-$result = $node_master->psql('postgres', "select application_name, sync_priority, sync_state from pg_stat_replication;");
+$result = $node_master->psql('postgres', "SELECT application_name, sync_priority, sync_state FROM pg_stat_replication;");
 print "$result \n";
 is($result, "standby2|2|sync\nstandby3|2|potential\nstandby1|1|sync\nstandby4|2|potential", 'checked for sync standbys state with asterisk 1');
 
 $node_standby_4->stop;
 
-# change the s_s_names = '2[*]' and check sync state
-$node_master->psql('postgres', "alter system set synchronous_standby_names = '2[*]';");
-$node_master->psql('postgres', "select pg_reload_conf();");
+# Change the s_s_names = '2[*]' and check sync state
+$node_master->psql('postgres', "ALTER SYSTEM SET synchronous_standby_names = '2[*]';");
+$node_master->psql('postgres', "SELECT pg_reload_conf();");
 
-$result = $node_master->psql('postgres', "select application_name, sync_priority, sync_state from pg_stat_replication;");
+$result = $node_master->psql('postgres', "SELECT application_name, sync_priority, sync_state FROM pg_stat_replication;");
 print "$result \n";
 is($result, "standby2|1|sync\nstandby3|1|sync\nstandby1|1|potential", 'checked for sync standbys state with asterisk 2');
 
-
-#Create some content on master and check its presence on standby 1 and standby 2
+# Create some content on master and check its presence on standby 1 and standby 2
 $node_master->psql('postgres', "CREATE TABLE tab_int AS SELECT generate_series(1,1002) AS a");
-
 
 $result =  $node_standby_1->psql('postgres', "SELECT count(*) FROM tab_int");
 print "standby 1: $result\n";
