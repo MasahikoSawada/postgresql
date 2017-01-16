83ea71e36266adf73962d6113a8279006df5669a
Masahiko Sawada <sawada.mshk@gmail.com>
2017-01-16 15:35:09 +0900

    Add TAP test.

--- src/test/recovery/t/009_fdw_xact.pl
+++ src/test/recovery/t/009_fdw_xact.pl
@@ -3,89 +3,91 @@ use strict;
 use warnings;
 use PostgresNode;
 use TestLib;
-use Test::More tests => 5;
-
-sub test_fdw_xact
-{
-	my ($self, $standby, $sql, $gxid, $expected, $msg) = @_;
-	my $result;
-
-	$self->safe_psql('postgres', $sql . "PREPARE TRANSACTION '$gxid'");
-
-	# test on master node
-	$result = $self->safe_psql('postgres', "SELECT count(*) FROM pg_fdw_xacts WHERE identifier = '$gxid'");
-	is($result, $expected, $msg);
-
-	# test on standby node
-	$result = $standby->safe_psql('postgres', "SELECT count(*) FROM pg_fdw_xacts");
-	is($result, $expected, $msg);
-}
+use Test::More tests => 9;
 
 # Setup master node
 my $node_master = get_new_node("maseter");
 my $node_standby = get_new_node("standby");
 
-my $port_coord = $node_master->port();
-
 $node_master->init(allows_streaming => 1);
 $node_master->append_conf('postgresql.conf', qq(
-	max_prepared_foreign_transactions = 10
-	max_prepared_transactions = 10
+max_prepared_foreign_transactions = 10
+max_prepared_transactions = 10
 ));
 $node_master->start;
 
-# Take backup
+# Take backup from master node
 my $backup_name = 'master_backup';
 $node_master->backup($backup_name);
 
 # Set up standby node
-$node_standby->init_from_backup($node_master, $backup_name);
-$node_standby->start();
+$node_standby->init_from_backup($node_master, $backup_name,
+	has_streaming => 1);
+$node_standby->start;
+
+# Set up foreign nodes
+my $node_fs1 = get_new_node("fs1");
+my $node_fs2 = get_new_node("fs2");
+my $fs1_port = $node_fs1->port;
+my $fs2_port = $node_fs2->port;
+$node_fs1->init;
+$node_fs2->init;
+$node_fs1->append_conf('postgresql.conf', "max_prepared_transactions = 10");		   
+$node_fs2->append_conf('postgresql.conf', "max_prepared_transactions = 10");		   
+$node_fs1->start;
+$node_fs2->start;
 
 # Create foreign server
-$node_master->safe_psql('postgres',
-					   qq[CREATE EXTENSION postgres_fdw]);
+$node_master->safe_psql('postgres', "CREATE EXTENSION postgres_fdw");
 $node_master->safe_psql('postgres', "
-CREATE SERVER loopback1 FOREIGN DATA WRAPPER postgres_fdw OPTIONS (dbname 'postgres', port '$port_coord', two_phase_commit 'off');
-CREATE SERVER loopback2 FOREIGN DATA WRAPPER postgres_fdw OPTIONS (dbname 'postgres', port '$port_coord', two_phase_commit 'on');
-CREATE SERVER loopback3 FOREIGN DATA WRAPPER postgres_fdw OPTIONS (dbname 'postgres', port '$port_coord', two_phase_commit 'on');
+CREATE SERVER fs1 FOREIGN DATA WRAPPER postgres_fdw
+OPTIONS (dbname 'postgres', port '$fs1_port', two_phase_commit 'on');
+");
+$node_master->safe_psql('postgres', "
+CREATE SERVER fs2 FOREIGN DATA WRAPPER postgres_fdw
+OPTIONS (dbname 'postgres', port '$fs2_port', two_phase_commit 'on');
 ");
 
 # Create user mapping
 $node_master->safe_psql('postgres', "
-CREATE USER MAPPING FOR CURRENT_USER SERVER loopback1;
-CREATE USER MAPPING FOR CURRENT_USER SERVER loopback2;
-CREATE USER MAPPING FOR CURRENT_USER SERVER loopback3;
+CREATE USER MAPPING FOR CURRENT_USER SERVER fs1;
+CREATE USER MAPPING FOR CURRENT_USER SERVER fs2;
 ");
 
-# Ceate table
+# Ceate table on foreign server and import them.
+$node_fs1->safe_psql('postgres', "
+CREATE SCHEMA fs;
+CREATE TABLE fs.t1 AS SELECT generate_series(1,10) AS c;
+");
+$node_fs2->safe_psql('postgres', "
+CREATE SCHEMA fs;
+CREATE TABLE fs.t2 AS SELECT generate_series(1,10) AS c;
+");
 $node_master->safe_psql('postgres', "
-CREATE SCHEMA sc1;
-CREATE TABLE sc1.t1 (c int);
-INSERT INTO sc1.t1 SELECT generate_series(1,10);
+IMPORT FOREIGN SCHEMA fs FROM SERVER fs1 INTO public;
+IMPORT FOREIGN SCHEMA fs FROM SERVER fs2 INTO public;
+CREATE TABLE local_table (c int);
+INSERT INTO local_table SELECT generate_series(1,10);
 ");
 
-# Create foreign table
-$node_master->safe_psql('postgres', "
-CREATE FOREIGN TABLE ft1 (c int) SERVER loopback1 OPTIONS (schema_name 'sc1', table_name 't1');
-CREATE FOREIGN TABLE ft2 (c int) SERVER loopback2 OPTIONS (schema_name 'sc1', table_name 't1');
-CREATE FOREIGN TABLE ft3 (c int) SERVER loopback3 OPTIONS (schema_name 'sc1', table_name 't1');
-");
+# Switch to synchronous replication
+$node_master->psql('postgres', "ALTER SYSTEM SET synchronous_standby_names = '*'");
+$node_master->reload;
 
 my $result;
 
 #
 # Prepare two transaction involving foreign servers.
-# Check if we can commit and rollback  transaction involving foreign servers after recovery.
+# Check if we can commit and rollback transaction involving foreign servers after recovery.
 #
 $node_master->safe_psql('postgres', "
 BEGIN;
-UPDATE ft2 SET c = 1 WHERE c = 1;
-UPDATE ft3 SET c = 2 WHERE c = 2;
+UPDATE t1 SET c = 1 WHERE c = 1;
+UPDATE t2 SET c = 2 WHERE c = 2;
 PREPARE TRANSACTION 'gxid1';
 BEGIN;
-UPDATE ft2 SET c = 3 WHERE c = 3;
-UPDATE ft3 SET c = 4 WHERE c = 4;
+UPDATE t1 SET c = 3 WHERE c = 3;
+UPDATE t2 SET c = 4 WHERE c = 4;
 PREPARE TRANSACTION 'gxid2';
 ");
 
@@ -98,17 +100,17 @@ $result = $node_master->psql('postgres', "ROLLBACK PREPARED 'gxid2'");
 is($result, 0, 'Rollback foreigin transaction after recovery');
 
 #
-# Prepare two transaction involving foreign servers and immediately shutdown.
+# Prepare two transaction involving foreign servers and shutdown master node immediately.
 # Check if we can commit and rollback transaction involving foreign servers after crash recovery.
 #
 $node_master->safe_psql('postgres', "
 BEGIN;
-UPDATE ft2 SET c = 5 WHERE c = 5;
-UPDATE ft3 SET c = 6 WHERE c = 6;
+UPDATE t1 SET c = 5 WHERE c = 5;
+UPDATE t2 SET c = 6 WHERE c = 6;
 PREPARE TRANSACTION 'gxid1';
 BEGIN;
-UPDATE ft2 SET c = 7 WHERE c = 7;
-UPDATE ft3 SET c = 8 WHERE c = 8;
+UPDATE t1 SET c = 7 WHERE c = 7;
+UPDATE t2 SET c = 8 WHERE c = 8;
 PREPARE TRANSACTION 'gxid2';
 ");
 
@@ -121,23 +123,64 @@ $result = $node_master->psql('postgres', "ROLLBACK PREPARED 'gxid2'");
 is($result, 0, 'Rollback foreigin transaction after crash recovery');
 
 #
-# Commit and rollback transactions involving foreign servers and immediately shutdown. In this
-# case, information about insertion and deletion of fdw_xact exists only WAL.
-# Check if the recovery can reply and resolve foreign transaciton.
+# Commit transactions involving foreign servers and shutdown master node immediately.
+# In this case, information about insertion and deletion of fdw_xact exists at only WAL.
+# Check if fdw_xact entry can be processed properly during recovery.
 #
 $node_master->safe_psql('postgres', "
 BEGIN;
-UPDATE ft2 SET c = 1 WHERE c = 1;
-UPDATE ft3 SET c = 2 WHERE c = 2;
+UPDATE t1 SET c = 1 WHERE c = 1;
+UPDATE t2 SET c = 2 WHERE c = 2;
 COMMIT;
-BEGIN;
-UPDATE ft2 SET c = 3 WHERE c = 3;
-UPDATE ft3 SET c = 4 WHERE c = 4;
-ROLLBACK;
 ");
 
 $node_master->teardown_node;
 $node_master->start;
 
-$result = $node_master->psql('postgres', 'SELECT count(*) FROM pg_fdw_xacts');
-is($result, 0, "Commit and rollback during recovery");
+$result = $node_master->safe_psql('postgres', 'SELECT count(*) FROM pg_fdw_xacts');
+is($result, 0, "Remove fdw_xact entry during recovery");
+
+#
+# A foreign server down after prepared foregin transaction but before commit it.
+# Check dangling transaction can be processed propelry by pg_fdw_xact() function.
+#
+$node_master->safe_psql('postgres', "
+BEGIN;
+UPDATE t1 SET c = 1 WHERE c = 1;
+UPDATE t2 SET c = 2 WHERE c = 2;
+PREPARE TRANSACTION 'gxid1';
+");
+
+$node_fs1->stop;
+
+# Since node_fs1 down COMMIT PREPARED will fail on node_fs1.
+$node_master->psql('postgres', "COMMIT PREPARED 'gxid1'");
+
+$node_fs1->start;
+$result = $node_master->safe_psql('postgres', "SELECT count(*) FROM pg_fdw_xact_resolve() WHERE status = 'resolved'");
+is($result, 1, "pg_fdw_xact_resolve function");
+
+#
+# Check if the standby node can process prepared foreign transaction after
+# promotion of the standby server.
+#
+$node_master->safe_psql('postgres', "
+BEGIN;
+UPDATE t1 SET c = 5 WHERE c = 5;
+UPDATE t2 SET c = 6 WHERE c = 6;
+PREPARE TRANSACTION 'gxid1';
+BEGIN;
+UPDATE t1 SET c = 7 WHERE c = 7;
+UPDATE t2 SET c = 8 WHERE c = 8;
+PREPARE TRANSACTION 'gxid2';
+");
+
+$node_master->teardown_node;
+$node_standby->promote;
+
+$result = $node_standby->psql('postgres', "COMMIT PREPARED 'gxid1'");
+is($result, 0, 'Commit foreigin transaction after promotion');
+$result = $node_standby->psql('postgres', "ROLLBACK PREPARED 'gxid2'");
+is($result, 0, 'Rollback foreigin transaction after promotion');
+$result = $node_standby->safe_psql('postgres', "SELECT count(*) FROM pg_fdw_xacts");
+is($result, 0, "Check fdw_xact entry on new master node");
