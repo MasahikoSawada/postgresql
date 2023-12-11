b68ba296803fecb2696d1dc07101c42308ea8b9d
Masahiko Sawada <sawada.mshk@gmail.com>
2023-12-11 22:04:01 +0900

    add missing copyapi.h

---
+++ src/include/commands/copyapi.h
@@ -0,0 +1,60 @@
+/*-------------------------------------------------------------------------
+ *
+ * copyapi.h
+ *	  API for COPY TO/FROM
+ *
+ * Copyright (c) 2015-2023, PostgreSQL Global Development Group
+ *
+ * src/include/command/copyapi.h
+ *
+ *-------------------------------------------------------------------------
+ */
+#ifndef COPYAPI_H
+#define COPYAPI_H
+
+typedef struct CopyFromStateData *CopyFromState;
+typedef struct CopyToStateData *CopyToState;
+
+typedef void (*CopyToStart_function) (CopyToState cstate, TupleDesc tupDesc);
+typedef void (*CopyToOneRow_function) (CopyToState cstate, TupleTableSlot *slot);
+typedef void (*CopyToEnd_function) (CopyToState cstate);
+
+/* XXX: just copied from COPY TO routines */
+typedef void (*CopyFromStart_function) (CopyFromState cstate, TupleDesc tupDesc);
+typedef void (*CopyFromOneRow_function) (CopyFromState cstate, TupleTableSlot *slot);
+typedef void (*CopyFromEnd_function) (CopyFromState cstate);
+
+typedef struct CopyToFormatRoutine
+{
+	NodeTag		type;
+
+	CopyToStart_function start_fn;
+	CopyToOneRow_function onerow_fn;
+	CopyToEnd_function end_fn;
+}			CopyToFormatRoutine;
+
+/* XXX: just copied from COPY TO routines */
+typedef struct CopyFromFormatRoutine
+{
+	NodeTag		type;
+
+	CopyFromStart_function start_fn;
+	CopyFromOneRow_function onerow_fn;
+	CopyFromEnd_function end_fn;
+}			CopyFromFormatRoutine;
+
+typedef struct CopyFormatRoutine
+{
+	NodeTag		type;
+
+	union
+	{
+		CopyToFormatRoutine copyto;
+		CopyFromFormatRoutine copyfrom;
+	}			routine;
+}			CopyFormatRoutine;
+
+extern CopyToFormatRoutine * GetCopyToFormatRoutine(char *format_name);
+extern CopyFromFormatRoutine * GetCopyFromFormatRoutine(char *format_name);
+
+#endif							/* COPYAPI_H */
